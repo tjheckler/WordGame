fcaa3066e951d72af09ba391fff25171dde28077
tjheckler <timothyheckler@gmail.com>
2018-05-18 16:42:40 -0500

    Silver almost done

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -4,6 +4,8 @@ import models.Words;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.util.*;
+
 public class GameController extends Controller
 {
     public Result getStart()
@@ -16,7 +18,74 @@ public class GameController extends Controller
         Words words = new Words();
         words.setPlace1("Parking Lot");
         words.setAdjective1("fuzzy");
+        words.setNoun1("toy");
+        words.setNoun2("ocean");
+        words.setVerbPastTense1("saved");
+        words.setPlace2(" the back yard");
 
+        return ok(views.html.story.render(words));
+    }
+    public Result getSecondStory()
+    {
+        Words words = new Words();
+        words.setPlace1("Beach");
+        words.setAdjective1("crazy");
+        words.setNoun1("sea shell");
+        words.setNoun2("bar");
+        words.setVerbPastTense1("punched");
+        words.setPlace2("the house");
+
+        return ok(views.html.story.render(words));
+    }
+    public Result getThirdStory()
+{
+    Words words = new Words();
+    words.setPlace1("School");
+    words.setAdjective1("fun");
+    words.setNoun1("cord");
+    words.setNoun2("classroom");
+    words.setVerbPastTense1("found");
+    words.setPlace2("works");
+
+    return ok(views.html.story.render(words));
+}
+    public Result getRandomStory()
+    {
+        Words words = new Words();
+        List nouns = new LinkedList();
+        nouns.add("man");
+        nouns.add("mountain");
+        nouns.add("state");
+        nouns.add("ocean");
+        nouns.add("bed");
+        nouns.add("cat");
+        nouns.add("country");
+
+        List verbs = new LinkedList();
+        verbs.add("become");
+        verbs.add("had broken");
+        verbs.add("brought");
+        verbs.add("chosen");
+        verbs.add("cut");
+        verbs.add("forgotten");
+        verbs.add("forgiven");
+
+        List adjectives = new LinkedList();
+        adjectives.add("creepy");
+        adjectives.add("restful");
+        adjectives.add("immense");
+        adjectives.add("helpful");
+        adjectives.add("pleasant");
+        adjectives.add("mysterious");
+        adjectives.add("tranquil");
+
+// finish variables below
+        Collections.shuffle(adjectives);
+        Collections.shuffle(nouns);
+        Collections.shuffle(verbs);
+        words.setAdjective1(((LinkedList) adjectives).getFirst().toString());
+        words.setNoun1(((LinkedList) nouns).getFirst().toString());
+        words.setVerbPastTense1(((LinkedList) nouns).getFirst().toString());
         return ok(views.html.story.render(words));
     }
 }
--- app/models/Words.java
+++ app/models/Words.java
@@ -1,9 +1,69 @@
 package models;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 public class Words
 {
+
+
+
     private String place1;
     private String adjective1;
+    private String noun1;
+    private String noun2;
+    private String verbPastTense1;
+    private String place2;
+   private List<String> nouns = new LinkedList<>();
+   private List<String> verbs = new LinkedList<>();
+   private List<String> adjectives = new LinkedList<>();
+
+
+
+    public List<String> getNouns()
+    {
+        return nouns;
+    }
+
+    public List<String> getVerbs()
+    {
+        return verbs;
+    }
+
+    public List<String> getAdjectives()
+    {
+        return adjectives;
+    }
+    public String getNoun2()
+    {
+        return noun2;
+    }
+
+    public void setNoun2(String noun2)
+    {
+        this.noun2 = noun2;
+    }
+
+    public String getVerbPastTense1()
+    {
+        return verbPastTense1;
+    }
+
+    public void setVerbPastTense1(String verbPastTense1)
+    {
+        this.verbPastTense1 = verbPastTense1;
+    }
+
+    public String getPlace2()
+    {
+        return place2;
+    }
+
+    public void setPlace2(String place2)
+    {
+        this.place2 = place2;
+    }
 
     public String getPlace1()
     {
@@ -15,7 +75,18 @@ public class Words
         this.place1 = place1;
     }
 
+    public String getNoun1()
+    {
+        return noun1;
+    }
+
+    public void setNoun1(String noun1)
+    {
+        this.noun1 = noun1;
+    }
+
     public String getAdjective1()
+
     {
         return adjective1;
     }
